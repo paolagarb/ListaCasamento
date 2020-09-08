62ad459121ec33adace55d2bc3d7fa17b5cf9e39
Paola Garbato <paolagarbato@gmail.com>
2020-09-08 19:09:08 -0300

    Finalizando lista de casamento

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var li = document.querySelector('#lista ul')
 var item = document.getElementById('item')
 var adicionar = document.querySelector('#lista button')
 var lista = [ 
-    'Jogo de panelas',
-    'Faqueiro',
-    'Porta corpo'
+    'Jogo de panelas ',
+    'Faqueiro ',
+    'Porta copo '
 ]
 
 function lerLista() {
@@ -12,21 +12,31 @@ function lerLista() {
     for (list of lista) {
         var itemLista = document.createElement('li')
         var texto = document.createTextNode(list)
+        var del =  document.createElement('a')
+        var delTexto = document.createTextNode('[X]')
+        var pos = lista.indexOf(list)
+
+        del.setAttribute('href', '#')
+        del.setAttribute('onClick', 'deletarItem(' + pos + ')')
 
         itemLista.appendChild(texto)
         li.appendChild(itemLista)
+        del.appendChild(delTexto)
+        itemLista.appendChild(del)
     }
 }
 
 lerLista()
 
-
 adicionar.onclick = function() {
-    var text = item.value
+    var text = item.value + " "
     lista.push(text)
     item.value = ""
     item.focus()
     lerLista()
-    
+}
 
+function deletarItem(pos) {
+    lista.splice(pos, 1); //a partir da posição indicada, deletar 1.
+    lerLista();
 }
